fe9584f760873b503d65e5c74a08980abde058b8
lesperon <luis.esperon@4thsource.com>
2019-11-06 12:26:41 -0600

    Add project files.

---
+++ AutomationSession7/Program.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationSession7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver;
+            
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
+
+           
+        }
+    }
+}
